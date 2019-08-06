a56e92208a31f48ea26e6f28977019d8f487e9bb
= <=>
2019-08-06 11:19:42 -0300

    Teste de commit com email da accenture

--- App.js
+++ App.js
@@ -128,13 +128,14 @@ export default class App extends Component {
     );
   }
 }
-
+//Adicionado Color para teste de commit.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    color:"#fff"
   },
  
 });
